70fb23ab8ea392f40ed6ab54212010e4659ab442
Sumit Vishnoi <sumitvishnoi7014@gmail.com>
2026-03-14 23:16:25 +0530

    create get-me and login api with express validator and middleware

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,5 +1,6 @@
 import userModel from "../models/user.model.js"
 import { sendEmail } from "../services/mail.service.js"
+import jwt from "jsonwebtoken"
 
 
 export async function register(req, res) {
@@ -26,12 +27,19 @@ export async function register(req, res) {
         password
     })
 
+    const emailVerificationToken = jwt.sign(
+        {email: user.email}, 
+        process.env.JWT_SECRET
+    )
+
     await sendEmail({
         to: email,
         subject: "Welcome to Perplexity!",
         html: `
             <p>Hi ${username}, </p>
             <p>Thank you for registering at Perplexity. We're excited to have you on board!</p>
+            <p>To get started, please verify your email address by clicking the link below:</p>
+             <a href="http://localhost:3000/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
             <p>Best regards,</p>
             <p>The Perplexity Team</p>
         `
@@ -45,4 +53,113 @@ export async function register(req, res) {
             email: user.email,
         }
     })
+}
+
+export async function login(req, res) {
+    const {email, password} = req.body
+
+    const user = await userModel.findOne({email})
+
+    if(!user) {
+        return res.status(404).json({
+            message: "Invalid email or password",
+            success: false,
+            err: "User not found"
+        })
+    }
+
+    const isPasswordMatch = await user.comparePassword(password)
+
+    if(!isPasswordMatch) {
+        return res.status(400).json({
+            message: "Invalid email or password",
+            success: false,
+            err: "Incorrect password"
+        })
+    }
+
+    if(!user.verified) {
+        return res.status(400).json({
+            message: "Please verify your email before login",
+            success: false,
+            err: "Email not verified"
+        })
+    }
+
+    const token = jwt.sign(
+        {id: user._id,
+            username: user.username
+        },
+        process.env.JWT_SECRET,
+        {expiresIn: "7d"}
+    )
+
+    res.cookie("token", token)
+
+    res.status(200).json({
+        message: "Login successful",
+        success: true,
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+}
+
+export async function getMe(req, res) {
+    const userId = req.user.id
+
+    const user = await userModel.findById(userId).select("-password")
+
+    if(!user) {
+        return res.status(404).json({
+            message: "User not found",
+            success: false,
+            err: "User not found"
+        })
+    }
+
+    res.status(200).json({
+        message: "User details fetched successfully",
+        success: true,
+        user
+    })
+}
+
+export async function verifyEmail(req, res) {
+    const {token} = req.query
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await userModel.findOne({email: decoded.email})
+
+        if(!user) {
+            return res.status(400).json({
+                messaage: "Invalid token",
+                success: false,
+                err: "User not found"
+            })
+        }
+
+        user.verified = true;
+
+        await user.save();
+
+        const html = `
+            <h1>Email Verified Successfully</h1>
+            <p>Your email has been verified successfully. You can now log in to your accound.</p>
+            <a href="http://localhost:3000/login">Go to Login</a>
+        `
+
+        return res.send(html)
+
+    } catch (error) {
+        return res.status(400).json({
+            message: "Invalid or expired token",
+            success: false,
+            err: error.message
+        })
+    }
 }
--- src/validators/auth.validator.js
+++ src/validators/auth.validator.js
@@ -28,4 +28,16 @@ export const registerValidator = [
     .isLength({min: 6}).withMessage("password must be at least 6 characters long"),
 
     validate
+] 
+
+export const loginValidator = [
+    body("email")
+    .trim()
+    .notEmpty().withMessage("Email is required")
+    .isEmail().withMessage("Please provide a valid email"),
+
+    body("password")
+    .notEmpty().withMessage("Password is required"),
+
+    validate
 ]
